24c2293ae4254f5cb3197fe686c77aecb7889828
ROLANDO ANDRADE <2712072023@mail.utec.edu.sv>
2024-06-06 20:57:59 -0400

    changes all Index Views

--- TEMIS/Controllers/CasosController.cs
+++ TEMIS/Controllers/CasosController.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -17,13 +19,75 @@ namespace TEMIS.Controllers
         private TEMISContext db = new TEMISContext();
 
         // GET: Casos
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string filtroActual, string cadenaBuscar, int? pagina)
         {
-            return View(db.Casos.ToList());
+            // parámetro de ordenamiento en el campo "Apellidos"
+            // y lo enviamos a la vista
+            ViewBag.CasoNombreSortParam = String.IsNullOrEmpty(sortOrder) ? "apellido_desc" : "";
+
+            //Enviar parametro "sortOrder" a la vista 
+            ViewBag.OrdenamiendoActual = sortOrder;
+
+            // parámetro de ordenamiento en el campo "facturacion"
+            // y lo enviamos a la vista
+            ViewBag.TipoFacturacionSortParam = sortOrder == "fact_asc" ? "fact_desc" : "fact_asc";
+            // variable para el listado de todos los abogados
+            // usando LINQ
+            var casos = from s in db.Casos select s;
+
+
+            //Definir la busqueda
+            if (!string.IsNullOrEmpty(cadenaBuscar))
+            {
+                //asignar al listado abogados el resultado de la consulta
+                casos = casos.Where(s => s.Caso_Nombre.Contains(cadenaBuscar) || s.Tipo_Facturacion.Contains(cadenaBuscar));
+            }
+
+            //definir la paginacion
+            if (cadenaBuscar != null)
+            {
+                pagina = 1;
+            }
+            else
+            {
+                cadenaBuscar = filtroActual;
+            }
+            //definir otro parametro de busqueda para  enviarlo a la vista
+            ViewBag.FiltroActual = cadenaBuscar;
+
+            switch (sortOrder)
+            {
+                // ordenamiento descendente por "Nombre en el caso"
+                case "apellido_desc":
+                    casos = casos.OrderByDescending(s => s.Caso_Nombre);
+                    break;
+
+                // ordenamiento descendente por "Tipo de Facturacion"
+                case "dui_desc":
+                    casos = casos.OrderByDescending(s => s.Tipo_Facturacion);
+                    break;
+
+                // ordenamiento ascendente por "Precio"
+                case "dui_asc":
+                    casos = casos.OrderBy(s => s.PrecioCaso);
+                    break;
+
+                // ordenamiento ascendente por "Apellidos"
+                default:
+                    casos = casos.OrderBy(s => s.Caso_Nombre);
+                    break;
+            }
+            //definir el tamaño de la pagina y la cantidad de paginas
+            int PageSize = 5;
+            int PageNumber = (pagina ?? 1);
+
+            //return View(db.Abogados.ToList());
+            //return View(abogados.ToList()); Ya no se puede usar este return
+            return View(casos.ToPagedList(PageNumber, PageSize));
         }
 
-        // GET: Casos/Details/5
-        public ActionResult Details(string id)
+    // GET: Casos/Details/5
+    public ActionResult Details(string id)
         {
             if (id == null)
             {
